640fe06e29e70972d124d31471f9e8234fa345e3
Daniel Perkins <daniel.perkins@panerabread.com>
2021-04-09 17:21:55 -0500

    initial commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="event-card">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: {
+      Object,
+      required: true,
+    }
+  },
+};
+</script>
+
+<style scoped>
+  .event-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+  }
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<script>
+import EventCard from '../components/EventCard'
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      
+    }
+  }
+};
+</script>
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
