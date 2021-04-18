f2900ed6f2c58bf6602f107c057da865050543f6
Daniel Perkins <daniel.perkins@panerabread.com>
2021-04-18 17:28:07 -0500

    made a simple form

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <label v-if="label"> {{ label }}</label>
+  <input
+    v-bind="$attrs"
+    class="field"
+    :placeholder="label"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+  modelBalue: {
+    type: [String, Number],
+    default: '',
+  },
+}
+</script>
+
+<style scoped>
+  .field {
+    box-shadow: 5px;
+    border: black;
+    height: 30px;
+    width: 250px;
+  }
+</style>
